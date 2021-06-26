17d91b38652d9ea98f29e40b52fe5f33b2578639
Philip <philip@smartace.ng>
2021-06-26 12:47:32 +0100

    Updated Designs

--- src/app/modules/applicants/pricing/pricing.component.ts
+++ src/app/modules/applicants/pricing/pricing.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent implements OnInit {
-
+  jobSeekerStatus: boolean = true;
+  employersStatus: boolean = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  toggleJobSeekers(){
+    this.jobSeekerStatus = true;
+    this.employersStatus = false;
+  }
+
+  toggleEmpoyers(){
+    this.employersStatus = true;
+    this.jobSeekerStatus = false;
+
   }
 
 }
